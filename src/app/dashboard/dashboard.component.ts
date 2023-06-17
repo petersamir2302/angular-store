bbb98aa8b85dc80f49b979d686885f2dfda6d50e
Peter Samir <petersamirnazir@gmail.com>
2023-06-17 18:44:28 +0300

    add pagination

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { Product } from '../product.model';
 import { ProductService } from '../products.service';
 import { ProductDialogComponent } from '../product-dialog/product-dialog.component';
@@ -11,19 +14,27 @@ import { ProductDialogComponent } from '../product-dialog/product-dialog.compone
 })
 export class DashboardComponent implements OnInit {
   products: Product[] = [];
+  dataSource: MatTableDataSource<Product> = new MatTableDataSource<Product>();
+  displayedColumns: string[] = ['name', 'description', 'price', 'actions'];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor(
     private productService: ProductService,
     private dialog: MatDialog
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.fetchProducts();
   }
 
   fetchProducts(): void {
-    this.productService.getProducts().subscribe(products => {
+    this.productService.getProducts().subscribe((products) => {
       this.products = products;
+      this.dataSource.data = this.products;
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
     });
   }
 
@@ -40,10 +51,12 @@ export class DashboardComponent implements OnInit {
       if (result) {
         this.productService.addProduct(result).subscribe(newProduct => {
           this.products.push(newProduct);
+          this.dataSource.data = this.products;
         });
       }
     });
   }
+
   openEditDialog(product: Product): void {
     const dialogRef = this.dialog.open(ProductDialogComponent, {
       width: '400px',
@@ -52,26 +65,30 @@ export class DashboardComponent implements OnInit {
         product: { ...product }
       }
     });
-  
+
     dialogRef.afterClosed().subscribe(result => {
       if (result) {
         this.productService.updateProduct(result.id, result).subscribe(updatedProduct => {
           const index = this.products.findIndex(p => p.id === updatedProduct.id);
           if (index !== -1) {
-            // Replace the existing product object with the updated product
             this.products[index] = { ...updatedProduct };
-            // Trigger change detection explicitly
-            this.products = [...this.products];
+            this.dataSource.data = this.products;
           }
         });
       }
     });
   }
-  
 
   deleteProduct(productId: number): void {
     this.productService.deleteProduct(productId).subscribe(() => {
       this.products = this.products.filter(p => p.id !== productId);
+      this.dataSource.data = this.products;
     });
+  }
+
+  onPageChange(event: any): void {
+    this.paginator.pageIndex = event.pageIndex;
+    this.paginator.pageSize = event.pageSize;
+    this.paginator.page.emit(event);
   }
 }
